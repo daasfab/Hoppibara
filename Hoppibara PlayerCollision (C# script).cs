917f015bae71c8f676a29c838bba1f046d9b27b2
Daulet <95215177+daasfab@users.noreply.github.com>
2024-02-05 22:46:57 +0000

    Add files via upload

---
+++ Hoppibara PlayerCollision (C# script).cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.tag == "Obstacle")
+        {
+            Destroy(gameObject);
+            //Game Manager Set Game Over
+        }
+    }
+}
