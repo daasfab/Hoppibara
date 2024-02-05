c9ddafceabcb8ff0449aec7196f1fd1bc2898565
Daulet <95215177+daasfab@users.noreply.github.com>
2024-02-05 22:46:28 +0000

    Add files via upload
    Code that will spawn the obstacles in random order procedurally

---
+++ Hoppibara Spawner (C# script).cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] obstaclePrefabs;
+    public float obstacleSpawnTime = 2f;
+    private float timeUntilObstacleSpawn;
+    public float obstacleSpeed = 1f;
+
+    private void Update()
+    {
+        SpawnLoop();
+    }
+
+    private void SpawnLoop()
+    {
+        timeUntilObstacleSpawn += Time.deltaTime;
+
+        if (timeUntilObstacleSpawn >= obstacleSpawnTime)
+        {
+            Spawn();
+            timeUntilObstacleSpawn = 0f;
+        }
+    }
+
+    private void Spawn()
+    {
+        GameObject obstacleToSpawn = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+
+        GameObject spawnedObstacle = Instantiate(obstacleToSpawn, transform.position, Quaternion.identity);
+
+        Rigidbody2D obstacleRB = spawnedObstacle.GetComponent<Rigidbody2D>();
+        obstacleRB.velocity = Vector2.left * obstacleSpeed;
+    }
+}
+
